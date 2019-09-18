291045131f21e772470958b1a9d66a704ad6bafa
Maxime Cattet <maximecattet@gmx.com>
2019-09-18 17:20:54 +0200

    :bug: Fix access level of functions

--- OnboardFlow/Classes/OnboardFlowViewController.swift
+++ OnboardFlow/Classes/OnboardFlowViewController.swift
@@ -86,11 +86,11 @@ open class OnboardFlowViewController: UIPageViewController {
         super.init(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
     }
     
-    required init?(coder: NSCoder) {
+    required public init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override public func viewDidLoad() {
+    override open func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.addSubview(self.pageControl)
